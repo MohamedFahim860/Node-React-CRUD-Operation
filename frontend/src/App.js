b59104a114107e225a733d6c37f58d34ddd42ddb
Fahim <fahimbm105@gmail.com>
2023-12-09 15:25:26 +0530

    Completed Backend and Frontend for the CRUD Operations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,18 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './Home'
 import Create from './Create'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Read from './Read'
+import Update from './Update'
+
 function App(){
   return(
     <BrowserRouter>
       <Routes>
         <Route path='/' element={ <Home/>} />
         <Route path='/create' element={ <Create/> }/>
+        <Route path='/read/:id' element={ <Read/> }/>
+        <Route path='/edit/:id' element={ <Update/> }/>
+
       </Routes>
     </BrowserRouter>
   )
